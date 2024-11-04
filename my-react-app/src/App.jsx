6b8c756f515c03bd7868db7f679fd1c22116bf48
NTS <slweetlov4@gmail.com>
2024-11-04 16:59:35 +0700

    Create App

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Popular from './Components/Popular';
+
+function App() {
+ return(
+    <div className="App">
+      <header className="App-header">
+        <input type="text" name="" id="" />
+        <Popular/>
+      </header>
+    </div>
+  );
+}
+
+export default App
---
+++ my-react-app/src/Components/Popular.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Popular() {
+  return (
+    <h1>Hello.</h1>
+  )
+};
+
+export default Popular;
