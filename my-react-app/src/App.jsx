9ea8a0bda11ed77f7b2f329cf508829e68b6d54b
NTS <slweetlov4@gmail.com>
2024-11-08 14:58:51 +0700

    Set up the route to the anime

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,14 +2,20 @@
 import Popular from './Components/Popular';
 import GlobalStyle from './Globalstyle';
 import { useGlobalContext } from './context/global';
-
+import { BrowserRouter, Route } from "react-router-dom"
 
 function App() {
   return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Popular />} />
+        <Route path="/anime/:id" element={<AnimeItem/>}/>
+      </Routes>
+      <div className="App">
+        <Popular />
+      </div>
+    </BrowserRouter>
     
-    <div className="App">
-      <Popular />
-    </div>
   );
   
 }
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -11,6 +11,7 @@ createRoot(document.getElementById('root')).render(
     <GlobalContextProvider>
       <App/>
     </GlobalContextProvider>
+    
   </StrictMode>
 )
 
